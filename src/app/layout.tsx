be38aa130a08fce715279742f379d498a8cbda17
AbhaY <pratapabhay540@gmail.com>
2024-07-14 07:14:45 +0530

    Asset update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,20 @@ import { TRPCReactProvider } from '~/trpc/react'
 export const metadata: Metadata = {
   title: 'Avani AI',
   description: 'Shopping agent',
-  icons: [{ rel: 'icon', url: '/favicon.ico' }],
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang='en'>
+      <head>
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff' />
+      </head>
       <body className={GeistSans.className}>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
